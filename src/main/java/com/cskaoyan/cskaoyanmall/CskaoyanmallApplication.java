007925220604426036151a30371cffee7830e938
chq <595224644@qq.com>
2020-05-28 16:08:11 +0800

    逆向工程生成javabean mapper mapper.xml

---
+++ src/main/java/com/cskaoyan/cskaoyanmall/CskaoyanmallApplication.java
@@ -0,0 +1,13 @@
+package com.cskaoyan.cskaoyanmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CskaoyanmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CskaoyanmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cskaoyan/cskaoyanmall/CskaoyanmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cskaoyan.cskaoyanmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CskaoyanmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
