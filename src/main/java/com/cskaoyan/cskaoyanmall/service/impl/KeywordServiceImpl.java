74fabae349164565847b7fdd445b7fc32b6f7066
wangxs666 <512393444@qq.com>
2020-05-30 22:49:19 +0800

    wangxs pom.xml delete konghang

---
+++ src/main/java/com/cskaoyan/cskaoyanmall/service/impl/KeywordServiceImpl.java
@@ -0,0 +1,49 @@
+package com.cskaoyan.cskaoyanmall.service.impl;
+
+import com.cskaoyan.cskaoyanmall.bean.Keyword;
+import com.cskaoyan.cskaoyanmall.bean.KeywordExample;
+import com.cskaoyan.cskaoyanmall.bean.KwListRespVo;
+import com.cskaoyan.cskaoyanmall.mapper.KeywordMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class KeywordServiceImpl implements com.cskaoyan.cskaoyanmall.service.KeywordService {
+
+    @Autowired
+    KeywordMapper keywordMapper;
+
+    @Override
+    public KwListRespVo list(Integer page, Integer limit, String sort, String order) {
+        KwListRespVo kwListRespVo = new KwListRespVo();
+        //kwListRespVo.setTotal((Long) keywordMapper.countByExample(new KeywordExample()));
+
+        KeywordExample keywordExample1 = new KeywordExample();
+        keywordExample1.setOrderByClause(sort + " " + order);
+        List<Keyword> keywordList = keywordMapper.selectByExample(keywordExample1);
+        //开始分页
+        PageHelper.startPage(page, limit);
+        PageInfo<Keyword> pageInfo = new PageInfo<>(keywordList);
+        long total = pageInfo.getTotal();
+
+        kwListRespVo.setTotal(total);
+        kwListRespVo.setItems(keywordList);
+        return kwListRespVo;
+    }
+
+    @Override
+    public Keyword create(String keyword, String url, Boolean isHot, Boolean isDefault) {
+
+        Keyword keyword1 = new Keyword();
+        keyword1.setKeyword(keyword);
+        keyword1.setUrl(url);
+        keyword1.setIsHot(isHot?0:1);
+        keywordMapper.insert(keyword1);
+
+        return null;
+    }
+}
